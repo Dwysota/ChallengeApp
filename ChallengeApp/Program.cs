31b6219baf5e2440dd5840e1aaf35537f8f71f3d
dwyso <dwyso@DESKTOP-699H4GE>
2023-03-25 11:48:23 +0100

    dzien 4 - poprawka

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,15 +1,41 @@
 ﻿String name = "Ewa";
-char sex = 'M'; // K - kobieta, M - mezczyzna
-int age = 17;
+char sex = 'K'; // K - kobieta, M - mezczyzna
+int age = 33;
 
-if (sex == 'K')
+if (age >= 0)
 {
-    if (name == "Ewa" && age == 33) Console.WriteLine(name + ", lat " + age);
-    else if (age < 33) Console.WriteLine("Kobieta ponizej 30 lat.");
-    else Console.WriteLine("Kobieta o imieniu " + name + ", lat " + age);
+    if (sex == 'K')
+    {
+        if (name == "Ewa" && age == 33)
+        {
+            Console.WriteLine(name + ", lat " + age);
+        }
+        else if (age < 33)
+        {
+            Console.WriteLine("Kobieta ponizej 33 lat.");
+        }
+        else
+        {
+            Console.WriteLine("Kobieta o imieniu " + name + ", lat " + age);
+        }
+    }
+    else if (sex == 'M')
+    {
+        if (age < 18)
+        {
+            Console.WriteLine("Niepelnoletni mezczyzna");
+        }
+        else
+        {
+            Console.WriteLine("Mezczyzna, o imieniu " + name + ", lat " + age);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Niepoprawna plec (M - mezczyzna, K - kobieta)");
+    }
 }
 else
 {
-    if (age < 18) Console.WriteLine("Niepelnoletni mezczyzna");
-    else Console.WriteLine("Mezczyzna, o imieniu " + name + ", lat " + age);
+    Console.WriteLine("Niepoprawny wiek (wiek nie moze byc ujemny");
 }
