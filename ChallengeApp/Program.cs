9aa9570b8de4ed5119e9823fca4f56ef8c8ecfe8
dwyso <dwyso@DESKTOP-699H4GE>
2023-03-25 11:19:37 +0100

    dzien 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String name = "Ewa";
+char sex = 'M'; // K - kobieta, M - mezczyzna
+int age = 17;
+
+if (sex == 'K')
+{
+    if (name == "Ewa" && age == 33) Console.WriteLine(name + ", lat " + age);
+    else if (age < 33) Console.WriteLine("Kobieta ponizej 30 lat.");
+    else Console.WriteLine("Kobieta o imieniu " + name + ", lat " + age);
+}
+else
+{
+    if (age < 18) Console.WriteLine("Niepelnoletni mezczyzna");
+    else Console.WriteLine("Mezczyzna, o imieniu " + name + ", lat " + age);
+}
