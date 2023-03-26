89c5f5d426c3f9cc5d6b524f177928e922230911
dwyso <dwyso@DESKTOP-699H4GE>
2023-03-26 16:24:30 +0200

    dzien 6

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -2,19 +2,18 @@
 using ChallengeApp;
 
 List<Employee> employees = new List<Employee> { new("Adam", "K", 35), new("Dawid", "W", 36), new("Agnieszka", "O", 33) };
-
-foreach (Employee employee in employees)
-{
-    employee.addScore(new Random().Next(1, 11));
-    employee.addScore(new Random().Next(1, 11));
-    employee.addScore(new Random().Next(1, 11));
-}
 Employee bestEmpolyee = new();
 foreach (Employee employee in employees)
 {
+    employee.addScore(new Random().Next(1, 11));
+    employee.addScore(new Random().Next(1, 11));
+    employee.addScore(new Random().Next(1, 11));
+    employee.addScore(new Random().Next(1, 11));
+    employee.addScore(new Random().Next(1, 11));
     if (bestEmpolyee.Result < employee.Result)
     {
         bestEmpolyee = employee;
     }
 }
+
 Console.WriteLine("Pracownik z najwieksza iloscia punktow: \n " + bestEmpolyee.getData());
